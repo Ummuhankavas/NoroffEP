fdbc5b62fdb6d0ee35032895c4582d114647d2f5
UmmuhanKavas <ummuhankavas@gmail.com>
2024-06-03 16:55:07 +0200

    server.js and config.js edited and connected to mysql db.

---
+++ e-commerce-app/api/config/config.js
@@ -0,0 +1,10 @@
+const config = {
+    db: {
+        host: "localhost",
+        user: "root",
+        password: "Nisan1453.",
+        database:"ecommerce"
+    },
+}
+
+module.exports = config
--- e-commerce-app/api/models/index.js
+++ e-commerce-app/api/models/index.js
@@ -8,6 +8,7 @@ const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
+const User = require('./user.model');
 
 let sequelize;
 if (config.use_env_variable) {
