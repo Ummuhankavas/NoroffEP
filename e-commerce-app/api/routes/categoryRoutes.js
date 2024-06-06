f417c085590abbf71404d1c7ac8ecb8e125cb70c
UmmuhanKavas <ummuhankavas@gmail.com>
2024-06-06 10:08:12 +0200

    categoryRoutes.js created

---
+++ e-commerce-app/api/routes/categoryRoutes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const { createCategory, getAllCategories, updateCategory, deleteCategory } = require('../controllers/categoryController');
+const authenticate = require('../middleware/authenticate');
+const authorize = require('../middleware/authorize');
+
+const router = express.Router();
+
+router.post('/', authenticate, authorize(['admin']), createCategory);
+router.get('/', getAllCategories);
+router.put('/:id', authenticate, authorize(['admin']), updateCategory);
+router.delete('/:id', authenticate, authorize(['admin']), deleteCategory);
+
+module.exports = router;
