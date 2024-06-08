f0f361aca46b3c7ced5bbd0371741c2fd3947904
UmmuhanKavas <ummuhankavas@gmail.com>
2024-06-08 22:54:11 +0200

    admin panel folder created

---
+++ e-commerce-app/admin/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Hello Norof Backend
+      </header>
+    </div>
+  );
+}
+
+export default App;
